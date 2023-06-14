13963c1cafab053c4ff3afedb45b346859fd5351
iTrooz <hey@itrooz.fr>
2023-06-14 22:26:45 +0200

    add get_partitions() backend for Linux

---
+++ bootedit/backend/partition_select/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+match sys.platform:
+    case "win32":
+        from .win32 import Disk, Partition, get_partitions
+    case "linux":
+        from .linux import Disk, Partition, get_partitions
+    case _:
+        raise RuntimeError("Platform not supported: "+sys.platform)
---
+++ bootedit/backend/partition_select/linux.py
@@ -0,0 +1,96 @@
+# This module retrieves partitions available on this computer and their name
+
+import os
+import subprocess
+from typing import Tuple, List, Optional
+
+class Disk:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.partitions = []
+
+    def __repr__(self) -> str:
+        return "Disk(name={}, partitions={})".format(self.name, self.partitions)
+
+class Partition:
+    def __init__(self, id, name: str) -> None:
+        self.id = id
+        self.name = name
+
+    def __repr__(self) -> str:
+        return "Partition(id={}, name={})".format(self.id, self.name)
+
+def trim_number(input_str: str):
+    """
+    Utility function to trim the number at the end of a string
+    Used for trimming numbers in Linux partition names (e.g. /dev/sda1 -> /dev/sda)
+    """
+    for i in reversed(range(len(input_str))):
+        if not input_str[i].isdigit():
+            return input_str[:i+1]
+    raise RuntimeError(f"string {input_str} has only numbers")
+
+def lsblk() -> dict:
+    parts_parents = {}
+
+    output = subprocess.check_output(['lsblk', '-o', 'pkname,name,type', '--raw'])
+
+    for line in output.split(b"\n"):
+        line = line.decode()
+        if not line:
+            continue
+
+        disk_name, part_name, type = line.split(" ")
+        if type == "part":
+            parts_parents[part_name] = disk_name
+    
+    return parts_parents
+
+def blkid() -> List[dict]:
+    parts_data = {}
+    partitions = [parts_data]
+
+    output = subprocess.check_output(['blkid', '-o', 'export'])
+
+    for line in output.split(b"\n"):
+        line = line.decode()
+        if not line:
+            partitions.append(parts_data)
+            parts_data = {}
+            continue
+        i = line.find("=")
+        key, value = line[:i], line[i+1:]
+
+        parts_data[key] = value
+    
+    return partitions
+    
+
+def get_partitions() -> Tuple[List[Disk], Optional[Partition]]:
+    """
+    Linux-only implementation
+    """
+
+    disks = {}
+
+    parts_parents = lsblk()
+
+    for part_data in blkid():
+        part_file = part_data["DEVNAME"]
+        part_uuid = part_data.get("PARTUUID")
+        if not part_uuid:
+            # not a partition
+            continue
+
+        # remove the '/dev/' part
+        part_name = part_file[5:]
+        disk_name = parts_parents[part_name]
+
+        disk = disks.get(disk_name)
+        if not disk:
+            disk = Disk(name=disk_name)
+            disks[disk_name] = disk
+
+        disk.partitions.append(Partition(id=part_file, name=part_file))
+    
+    return disks.values(), None
