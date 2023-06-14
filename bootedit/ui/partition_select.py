ad59cb9eccdcda27d47b66543d73fd610f95681c
iTrooz <hey@itrooz.fr>
2023-06-15 00:22:16 +0200

    get back Partition object when partition is clicked

--- bootedit/ui/partition_select.py
+++ bootedit/ui/partition_select.py
@@ -3,6 +3,7 @@ from typing import List
 from PyQt6.QtWidgets import *
 
 from bootedit.backend.partition_select import Disk, Partition
+from bootedit.backend.partition_select import mount, unmount
 
 class PartitionSelector(QWidget):
     
@@ -18,6 +19,7 @@ class PartitionSelector(QWidget):
 
         self.tree = QTreeWidget()
         self.tree.setHeaderHidden(True)
+        self.tree.doubleClicked.connect(self.partition_selected)
         layout.addWidget(self.tree)
 
         bottom_widget = QWidget()
@@ -29,6 +31,7 @@ class PartitionSelector(QWidget):
 
         self.open_button = QPushButton("Open")
         self.open_button.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        self.open_button.clicked.connect(self.partition_selected)
         bottom_layout.addWidget(self.open_button)
 
 
@@ -44,9 +47,26 @@ class PartitionSelector(QWidget):
 
             for partition in disk.partitions:
                 part_item = QTreeWidgetItem()
+                part_item.partition = partition
                 part_item.setText(0, partition.name)
                 disk_item.addChild(part_item)
 
                 if partition == default_partition:
                     part_item.setSelected(True)
+    
+    def partition_selected(self):
+        selected_list = self.tree.selectedItems()
+        if len(selected_list) != 1:
+            print("Should not happen: partition_selected() called not 1 selected item: {}", selected_list)
+            return
+        
+        part_item = selected_list[0]
+        self.selected_partition = part_item.partition
+        
+        root_folder = mount(self.selected_partition)
 
+        file = select_file(root_folder)
+
+        unmount(root_folder)
+
+        print("Selected: file")
