94e486be13e3bc036399ea3e4a03c92bd91fcb51
iTrooz <hey@itrooz.fr>
2023-06-17 20:38:16 +0200

    replace mount implementation: run commands instead of using glibc directly
    (To supports ntfs)

--- bootedit/backend/partition_select/linux/mount.py
+++ bootedit/backend/partition_select/linux/mount.py
@@ -1,29 +1,17 @@
-import ctypes
-import ctypes.util
+import subprocess
 import os
 import tempfile
 
 from .get_partitions import Partition
 
-libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
-libc.mount.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p)
-
-def mount_raw(source, target, fs, options=''):
-    ret = libc.mount(source.encode(), target.encode(), fs.encode(), 0, options.encode())
-    if ret < 0:
-        errno = ctypes.get_errno()
-        raise OSError(errno, f"Error mounting {source} ({fs}) on {target} with options '{options}': {os.strerror(errno)}")
-
 def mount(partition: Partition):
     tmpdir = tempfile.mkdtemp(prefix="tmp-bootedit-")
-    mount_raw(partition.device_name, tmpdir, partition.type, 'ro')
-
+    
+    subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type, '-r'])
+    
     print(f"Mounted partition {partition.device_name} on {tmpdir}")
 
     return tmpdir
 
-def unmount(device_path: str, options=0):
-    ret = libc.umount2(device_path.encode(), options)
-    if ret < 0:
-        errno = ctypes.get_errno()
-        raise OSError(errno, f"Error umounting {device_path} with options '{options}': {os.strerror(errno)}")
+def unmount(device_path: str):
+    subprocess.check_call(["umount", device_path])
