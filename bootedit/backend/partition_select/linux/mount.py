61ccb55bc4fb3c9593f8fc52d6892fc3abbab2ac
iTrooz <hey@itrooz.fr>
2023-06-17 21:24:04 +0200

    Actually throw MountError

--- bootedit/backend/partition_select/linux/mount.py
+++ bootedit/backend/partition_select/linux/mount.py
@@ -15,19 +15,17 @@ def mount(partition: Partition) -> str:
     """
     tmpdir = tempfile.mkdtemp(prefix="tmp-bootedit-")
     
-    mounted = False
-    try:
-        subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type, '-r'])
-        mounted = True
-    except subprocess.CalledProcessError:
-        pass
-    
     # mount in read-only can fail if the partition was already mounted in read-write elsewhere
     # If so, try to mount again in read-write mode
     # Do not do this in the 'except' block to avoid 'During handling of the above exception, another exception occurred'
-    if not mounted:
-        subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type])
-    
+    try:
+        subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type, '-r'])
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type])
+        except subprocess.CalledProcessError as e:
+            raise MountError(e) from None
+
     print(f"Mounted partition {partition.device_name} on {tmpdir}")
 
     return tmpdir
