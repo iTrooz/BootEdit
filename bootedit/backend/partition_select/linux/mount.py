f1d64c3a058dac3ac89473ef6ed8a2ec3cae30ac
iTrooz <hey@itrooz.fr>
2023-06-17 20:53:22 +0200

    try to mount in rw if we can't mount in ro

--- bootedit/backend/partition_select/linux/mount.py
+++ bootedit/backend/partition_select/linux/mount.py
@@ -7,7 +7,18 @@ from .get_partitions import Partition
 def mount(partition: Partition):
     tmpdir = tempfile.mkdtemp(prefix="tmp-bootedit-")
     
-    subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type, '-r'])
+    mounted = False
+    try:
+        subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type, '-r'])
+        mounted = True
+    except subprocess.CalledProcessError:
+        pass
+    
+    # mount in read-only can fail if the partition was already mounted in read-write elsewhere
+    # If so, try to mount again in read-write mode
+    # Do not do this in the 'except' block to avoid 'During handling of the above exception, another exception occurred'
+    if not mounted:
+        subprocess.check_call(["mount", partition.device_name, tmpdir, "-t", partition.type])
     
     print(f"Mounted partition {partition.device_name} on {tmpdir}")
 
