b865d1abd5fc0442f0feab78335934f67974c34f
iTrooz <hey@itrooz.fr>
2023-06-18 12:12:08 +0200

    refactor and comment everything, and make it work

--- bootedit/ui/entry_add.py
+++ bootedit/ui/entry_add.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import os
 
 from PyQt6.QtWidgets import *
@@ -40,72 +40,198 @@ class EntryAddWindow(QWidget):
         super().__init__(*kargs, **kwargs)
         
         self.selected_partition: Partition = None
-        self.selected_file_relpath: str = None
-        self.selected_file_size: int = None
-        self.entry_name: str = None
-        self.mount_point: str = ""
+        self.selected_file_relpath: Optional[str] = None
+        self.selected_file_size: Optional[int] = None
+        self.entry_name: str = ""
+        self.mount_point: Optional[str] = None
+        self.disks: Optional[List[Disk]] = None
 
         self.ui = Ui_AddUEFIEntry()
         self.ui.setupUi(self)
 
-        self.update_widgets_status()
-
-        self.ui.tree_manual_partition.itemSelectionChanged.connect(self.partition_selected)
-        self.ui.but_manual_file.clicked.connect(self.select_file)
-
+        # Reset UI values
+        self.ui.edit_partition.setText("")
+        self.ui.edit_file.setText("")
+        self.ui.edit_file_size.setText("")
         self.ui.edit_entry_name.setText("")
-        self.ui.edit_entry_name.textEdited.connect(lambda: self.entry_name_edited())
 
-    def info_entry_name_edited(self):
-        self.entry_name = self.ui.edit_entry_name.text()
-        self.update_widgets_status()
+        self.ui.tree_manual_partition.itemSelectionChanged.connect(self.partition_tree_selected_slot)
+        self.ui.edit_partition.textChanged.connect(self.partition_line_edit_edited_slot)
 
-    def set_partition_and_update(self, partition: Partition) -> None:
+        self.ui.but_manual_file.clicked.connect(self.select_file_with_file_selector_slot)
+        self.ui.edit_file.textChanged.connect(self.file_line_edit_edited_slot)
+
+        self.ui.edit_entry_name.textEdited.connect(self.entry_name_line_edit_edited_slot)
+
+        self.update_widgets_errors()
+
+    # ----- Partition stuff
+
+    def find_partition(self, device_name) -> Optional[Partition]:
+        for disk in self.disks:
+            for partition in disk.partitions:
+                if partition.device_name == device_name:
+                    return partition
+        return None
+
+    def partition_line_edit_edited_slot(self):
+        """
+        Trigerred when we edit the partition line edit widget (one call per character)
+        """
+
+        partition = self.find_partition(self.ui.edit_partition.text()) # Find this device name in all the partitions we have
+        self.set_selected_partition(partition) # sets self.selected_partition, mounts it, and clears file line edits
+
+        self.update_widgets_errors()
+
+    def partition_tree_selected_slot(self):
+        """
+        Trigerred when we select a partition through the tree widget
+        """
+
+        selected_list = self.ui.tree_manual_partition.selectedItems()
+        partition: Optional[Partition] = None
+        if len(selected_list) == 1:
+            part_item = selected_list[0]
+            if hasattr(part_item, "partition"):
+                partition = part_item.partition
+        
+        if partition:
+            self.ui.edit_partition.setText(partition.device_name)
+            # the edit line text edit event will take care of updating the partition
+            
+            self.update_widgets_errors()
+        else:
+            self.ui.edit_partition.setText("")
+            # the edit line text edit event will take care of updating the partition
+
+            self.update_widgets_errors()
+    
+    def set_selected_partition(self, partition: Optional[Partition]) -> None:
+        """
+        Helper function to set the partition, mount it, and clear file data
+        does NOT set the line edit
+        """
+
         if self.selected_partition == partition:
             return
-        
-        self.selected_partition = partition
+
+        # clear file data
+        self.set_selected_file(None)
+        self.update_file_line_edits()
 
         # remove from old mount point
         if self.mount_point:
             unmount(self.mount_point)
             self.mount_point = None
 
-        if self.selected_partition:
+        # try to mount the new mount point
+        error = None
+        if partition:
             try:
-                self.mount_point = mount(self.selected_partition)
+                self.mount_point = mount(partition)
             except MountError as e:
-                errmsg = (f"Failed to mount partition {self.selected_partition.device_name}.\n"
-                                        f"Error: {e}")
-                self.mount_point = None
-                self.selected_partition = None
-                self.set_file_and_update(None, None, update=False)
+                error = e
 
-                self.update_widgets_status()
+        if error:
+            self.mount_point = None
+            self.selected_partition = None
+            self.selected_partition_valid = False
 
-                QMessageBox.critical(self, "", errmsg)
-            
-                return
+            self.update_widgets_errors()
 
-        self.set_file_and_update(None, None, update=False)
+            QMessageBox.critical(self, "", f"Failed to mount partition {partition.device_name}.\n"
+                                    f"Error: {error}")
+        elif partition == None:
+            self.selected_partition = None
+            self.selected_partition_valid = False
 
-        self.update_widgets_status()
-
-    def set_file_and_update(self, file_full_path: str, file_rel_path: str, update: bool = True) -> None:
-        if self.selected_file_relpath == file_rel_path:
-            return
-        
-        if file_full_path and file_rel_path:
-            self.selected_file_size = os.path.getsize(file_full_path)
-            self.selected_file_relpath = file_rel_path
+            self.update_widgets_errors()
         else:
-            self.selected_file_size = 0
-            self.selected_file_relpath = ""
+            self.selected_partition = partition
+            self.selected_partition_valid = True
 
-        if update:
-            self.update_widgets_status()
+            self.update_widgets_errors()
 
-    def update_widgets_status(self):
+    # ----- END Partition stuff
+
+    # ----- File and file size stuff
+
+    def file_line_edit_edited_slot(self):
+        """
+        Trigerred when we edit the file line edit widget (one call per character)
+        """
+
+        full_file_path = os.path.join(self.mount_point, self.ui.edit_file.text())
+        self.set_selected_file(full_file_path)
+
+        self.update_widgets_errors()
+
+    def select_file_with_file_selector_slot(self):
+        """
+        Called when we want to select the file with the system file selector
+        """
+        ret = QFileDialog.getOpenFileName(directory=self.mount_point)
+
+        full_file_path = ret[0]
+        if full_file_path:
+            if path_is_parent(self.mount_point, full_file_path):
+                self.set_selected_file(full_file_path)
+
+                self.update_file_line_edits()
+
+                self.update_widgets_errors()
+            else:
+                
+                QMessageBox.critical(self, "", "File selected is not inside the mounted partition")
+
+    def update_file_line_edits(self):
+        """
+        Override edit line values with the object fields
+        This will NOT call events sets on the file edit line.
+        """
+
+        old_state = self.ui.edit_file.blockSignals(True)
+
+        if self.selected_file_relpath == None:
+            self.ui.edit_file.setText("")
+            self.ui.edit_file_size.setText("")
+        else:
+            self.ui.edit_file.setText(self.selected_file_relpath)
+            self.ui.edit_file_size.setText(sizeof_fmt(self.selected_file_size))
+        
+        self.ui.edit_file.blockSignals(old_state)
+
+    def set_selected_file(self, full_file_path: Optional[str]) -> None:
+        """
+        Helper function to set the selected file field and its size.
+        does NOT set the line edits
+        """
+
+        if full_file_path and os.path.isfile(full_file_path):
+            self.selected_file_size = os.path.getsize(full_file_path)
+            self.selected_file_relpath = os.path.relpath(full_file_path, self.mount_point)
+        else:
+            self.selected_file_size = None
+            self.selected_file_relpath = None
+
+    # ----- END File and file size stuff
+
+    # ----- Entry name stuff
+
+    def entry_name_line_edit_edited_slot(self):
+        self.entry_name = self.ui.edit_entry_name.text()
+
+        self.update_widgets_errors()
+
+    # ----- EDN Entry name stuff
+
+    def update_widgets_errors(self):
+        """
+        Update the red rectangle on line edits, button grayed, and the final error message.
+        Does NOT modify the edit lines.
+        """
+
         # Only enable the file selection button if we selected a partition
         self.ui.but_manual_file.setDisabled(self.selected_partition == None)
 
@@ -113,27 +239,28 @@ class EntryAddWindow(QWidget):
 
         # Update "Partition" line in "Entry information"
         if self.selected_partition:
-            self.ui.edit_partition.setText(self.selected_partition.device_name)
             clear_line_edit_color(self.ui.edit_partition)
         else:
-            error_msg = error_msg or "Please select a partition"
-            self.ui.edit_partition.setText("")
             set_line_edit_color(self.ui.edit_partition)
+            if self.ui.edit_partition.text():
+                error_msg = error_msg or "Invalid partition selected"
+            else:
+                error_msg = error_msg or "Please select a partition"
 
         # Update "File" line in "Entry information"
         if self.selected_file_relpath:
-            self.ui.edit_file.setText(self.selected_file_relpath)
             clear_line_edit_color(self.ui.edit_file)
         else:
-            self.ui.edit_file.setText("")
-            error_msg = error_msg or "Please select a file inside the partition"
-            set_line_edit_color(self.ui.edit_file)
-
-        # Update "File size" line in "Entry information"
-        if self.selected_file_size:
-            self.ui.edit_file_size.setText(sizeof_fmt(self.selected_file_size))
-        else:
-            self.ui.edit_file_size.setText("")
+            if self.selected_partition:
+                set_line_edit_color(self.ui.edit_file)
+                if self.ui.edit_file.text():
+                    error_msg = error_msg or "Invalid file selected"
+                else:
+                    error_msg = error_msg or "Please select a file inside the partition"
+            else:
+                # If a partition is not selected, we shouldn't even be able to edit the field
+                clear_line_edit_color(self.ui.edit_file)
+                self.ui.edit_file.setEnabled(False)
 
         # Set the error message is the entry name is not set
         if self.entry_name:
@@ -153,6 +280,8 @@ class EntryAddWindow(QWidget):
     
 
     def set_partitions_data(self, disks: List[Disk], default_partition: Partition):
+        self.disks = disks
+
         self.ui.tree_manual_partition.clear()
 
         for disk in disks:
@@ -171,26 +300,3 @@ class EntryAddWindow(QWidget):
 
                 if partition == default_partition:
                     part_item.setSelected(True)
-    
-    def partition_selected(self):
-        selected_list = self.ui.tree_manual_partition.selectedItems()
-        if len(selected_list) == 1:
-            part_item = selected_list[0]
-            if hasattr(part_item, "partition"):
-                self.set_partition_and_update(part_item.partition)
-                return
-            
-
-        self.set_partition_and_update(None)
-
-        
-    def select_file(self):
-        ret = QFileDialog.getOpenFileName(directory=self.mount_point)
-
-        selected_file = ret[0]
-        if selected_file:
-            if path_is_parent(self.mount_point, selected_file):
-                self.set_file_and_update(selected_file, os.path.relpath(selected_file, self.mount_point))
-            else:
-                QMessageBox.critical(self, "", "File selected is not inside the mounted partition")
-
