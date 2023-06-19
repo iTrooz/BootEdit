c1460b6b59dc1aa300043bb6acc3ff5c20c8b5f1
iTrooz <hey@itrooz.fr>
2023-06-19 20:45:52 +0200

    rename window variables in ApplicationLogic

--- bootedit/logic/application.py
+++ bootedit/logic/application.py
@@ -14,27 +14,26 @@ class ApplicationLogic:
         partition selector window. None else
     """
     def __init__(self):
-        self.partition_selector = None
         self.init_ui()
 
     def init_ui(self):
-        self.window = MainWindow()
+        self.main_window = MainWindow()
         self.reload_entries()
 
-        self.window.add_button.clicked.connect(lambda: self.show_add_entry_window())
+        self.main_window.add_button.clicked.connect(lambda: self.show_add_entry_window())
 
     def reload_entries(self) -> None:
-        self.window.set_entries(get_uefi_entries())
+        self.main_window.set_entries(get_uefi_entries())
 
     def show_add_entry_window(self):
-        self.entry_add = AddUEFIEntryWindow()
-        self.entry_add.set_partitions_data(*get_partitions())
-        self.entry_add.show()
-        self.entry_add.closeEvent = self.on_close_add_entry_window
+        self.add_entry_window = AddUEFIEntryWindow()
+        self.add_entry_window.set_partitions_data(*get_partitions())
+        self.add_entry_window.show()
+        self.add_entry_window.closeEvent = self.on_close_add_entry_window
 
     def on_close_add_entry_window(self, *args) -> None:
         self.reload_entries()
 
 
     def show_window(self):
-        self.window.show()
+        self.main_window.show()
